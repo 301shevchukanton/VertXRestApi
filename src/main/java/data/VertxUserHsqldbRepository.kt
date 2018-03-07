f6477cc5b11acd9b4a2219d98dd7682ad2d24298
Anton Shevchuk <ash@qapint.com>
2018-03-08 01:09:22 +0200

    added Hsqldb repo

---
+++ src/main/java/data/VertxUserHsqldbRepository.kt
@@ -0,0 +1,26 @@
+package data
+
+import io.vertx.ext.web.RoutingContext
+
+class VertxUserHsqldbRepository : VertxUserRepository {
+
+    override fun addOne(routingContext: RoutingContext) {
+
+    }
+
+    override fun getAll(routingContext: RoutingContext) {
+
+    }
+
+    override fun getOne(routingContext: RoutingContext) {
+
+    }
+
+    override fun updateOne(routingContext: RoutingContext) {
+
+    }
+
+    override fun deleteOne(routingContext: RoutingContext) {
+
+    }
+}
